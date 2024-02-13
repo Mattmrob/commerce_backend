8430c355be37cc4dcf7fbd9f9a1dc1417e9f6fae
Mysmelf <matthewr9937@gmail.com>
2024-02-13 12:27:55 -0800

    Added get product by id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,9 +16,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productById = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}],
+    });
+
+    if (!productById) {
+      res.status(404).json({ message: 'No Product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
