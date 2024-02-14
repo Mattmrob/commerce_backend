589812ab2d5b1c4642c3e2c1666dcfaf92b9ebef
Mysmelf <matthewr9937@gmail.com>
2024-02-14 11:13:56 -0800

    Created PUT route for tag updating

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -40,8 +40,29 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const UpdateById = await Tag.update(
+      {
+        tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    );
+
+    if (!UpdateById) {
+      res.status(404).json({ message: 'No Tag found with that id to update!' });
+      return;
+    }
+
+    res.status(200).json(UpdateById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
