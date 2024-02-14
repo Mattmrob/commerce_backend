fd9216cf89453871c3dd6f5002f829fdf560f369
Mysmelf <matthewr9937@gmail.com>
2024-02-14 11:21:02 -0800

    added delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -145,22 +145,22 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // try {
-  //   const catDeleteById = await Product.destroy({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   });
+  try {
+    const DeleteById = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
 
-  //   if (!catDeleteById) {
-  //     res.status(404).json({ message: 'No Category found with that id to delete!' });
-  //     return;
-  //   }
+    if (!DeleteById) {
+      res.status(404).json({ message: 'No Category found with that id to delete!' });
+      return;
+    }
 
-  //   res.status(200).json(catDeleteById);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+    res.status(200).json(DeleteById);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
